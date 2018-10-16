1caea14855a714753337fc9e057fe7daec83255f
WrightKim <wrightkim01@gmail.com>
2018-10-15 20:10:02 -0400

    ch1.1

--- ch1/ch1_1.cpp
+++ ch1/ch1_1.cpp
@@ -6,28 +6,16 @@ using namespace std;
  must print "input i number:"
  */
 int main (void){
-	/*
-	int a, b, c, d, e;
-	cout<<"type first number: "<<endl;
-	cin>>a;
-	cout<<"type second number: "<<endl;
-	cin>>b;
-	cout<<"type third number: "<<endl;
-	cin>>c;
-	cout<<"type fourth number: "<<endl;
-	cin>>d;
-	cout<<"type fifth number: "<<endl;
-	cin>>e;
-
-	int sum = a+b+c+d+e;
-	cout<<"sum = "<<sum<<endl;
-*/
+	
 	int input=0, num=0;
+	
 	for (int i=0; i<5; i++){
 		cout<<"input "<<i+1<<" number: ";
 		cin>>input;
 		num+=input;
 	}
+	
 	cout<<"sum= "<<num<<endl;
+	
 	return 0;
 }
